e24b64c276fa7cf312598e2d0972e6e56bded326
emjc-github <enrique.johnsonchumbes@hgs.hiddengeniusproject.org>
2024-10-30 17:05:24 -0400

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,11 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener(DOMContentLoaded",() => {
+     const modal = documentgetElementById("myModal");
+    const open = documentgetElementById("myModal");
+    const close = documentgetElementById("Modal");
+}
+    openModalButton.addEventListener("click", () => {
+    modal.style.display = "block";
+}
+    closeModalButton.addEventListener("click" , () => {
+    modal.style.display = "block";
+}
