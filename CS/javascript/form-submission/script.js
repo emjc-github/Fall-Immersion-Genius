dd667f1af3fae407c569e0bdbbada84826de4677
emjc-github <enrique.johnsonchumbes@hgs.hiddengeniusproject.org>
2024-10-30 17:53:54 -0400

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -5,9 +5,11 @@ form.addEventListener('submit', function(event)){
 event.preventDefault()
 
 form = document.getElementById("name")
-form = document.getElementById("")
-form
+form = document.getElementById("email")
+form = document.getElemnetById("message")
 
-    if()
+    if(document.getElementById("name") && document.getElementById("email") && document.getElemnetById("message"))
+        alert('Form submitted successfully!\nName: ${username}\nEmail...');
     }
+    else('Fill all the feilds\nName: ${username}\nEmail...');
 }
