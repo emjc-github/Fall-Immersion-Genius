e63e02b3754bdfd3da01b89e0870114ecf24a049
emjc-github <enrique.johnsonchumbes@hgs.hiddengeniusproject.org>
2024-10-31 16:59:54 -0400

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener('submit', function(event)){
         event.preventDefault()
 
-    name = document.getElementById("name")
-    email = document.getElementById("email")
-    message = document.getElemnetById("message")
+        username = document.getElementById("name")
+        email = document.getElementById("email")
+        message = document.getElemnetById("message")
 
-    if(document.getElementById("name") && document.getElementById("email") && document.getElemnetById("message"))
-        alert('Form submitted successfully!\nName: ${username}\nEmail...');
-    }
-    else(document.getElementById("name") && document.getElementById("email") && document.getElemnetById("message"))
-        alert('Fill all the feilds\nName: ${username}\nEmail...');
-}
+        if(("name") && ("email") && ("message"))
+        alert('Form submitted successfully!\nName: ${username.value}\nEmail: ${email.value}\nMessage: ${message.value}');
+        }
+        else(("name") && ("email") && ("message"))
+            alert('Fill all the feilds in\nName: ${username.value}\nEmail: ${email.value}\nMessage: ${message.value}');
+        }
