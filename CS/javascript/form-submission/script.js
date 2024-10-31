545cea790130f884099fc08082a8887349126378
emjc-github <enrique.johnsonchumbes@hgs.hiddengeniusproject.org>
2024-10-31 17:13:15 -0400

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-        const form = document.getElemntById("userInfo")
+    const form = document.getElemntById("userInfo");
 
     form.addEventListener('submit', function(event)){
         event.preventDefault()
 
-        username = document.getElementById("name")
-        email = document.getElementById("email")
-        message = document.getElemnetById("message")
+        username = document.getElementById("name");
+        email = document.getElementById("email");
+        message = document.getElemnetById("message");
 
-        if(("name") && ("email") && ("message"))
+        if(username && email && message){
             alert('Form submitted successfully!\nName: ${username.value}\nEmail: ${email.value}\nMessage: ${message.value}');
         }
-        else(("name") && ("email") && ("message"))
-            alert('Fill all the feilds in\nName: ${username.value}\nEmail: ${email.value}\nMessage: ${message.value}');
+        else{
+            alert('Fill all the fields in');
         }
     });
 });
