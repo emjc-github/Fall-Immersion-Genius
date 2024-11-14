89bd4eae1ea897c1e1cbbdfca782ae1851304787
emjc-github <enrique.johnsonchumbes@hgs.hiddengeniusproject.org>
2024-11-14 17:28:32 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,4 +1,5 @@
-document.getElementById("Clicker").addEventListener('click', function (){
-paragraph = document. createElement ('p');
-paragraph. textContent = "DON'T STOP CLICKING THE BUTTON!!!";
-document. body.Clicker (paragraph) ;
+document.getElementById("Clicker").addEventListener('click',function(){
+  image = document.createElement('img');
+  image.scr = "https://www.google.com/imgres?q=Dont%20Stop%20clicking%20the%20button&imgurl=https%3A%2F%2Fimages.slideplayer.com%2F10%2F2900728%2Fslides%2Fslide_125.jpg&imgrefurl=https%3A%2F%2Fslideplayer.com%2Fslide%2F2900728%2F&docid=pohy_hIvYVWmDM&tbnid=iW6QCRj_6H7o5M&vet=12ahUKEwizjIv97dyJAxWRMlkFHQv-NWYQM3oFCIABEAA..i&w=960&h=720&hcb=2&ved=2ahUKEwizjIv97dyJAxWRMlkFHQv-NWYQM3oFCIABEAA";
+  document.body.appendChild(image);
+});
