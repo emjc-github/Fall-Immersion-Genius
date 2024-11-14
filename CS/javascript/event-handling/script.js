33b291bfb1fc857c6e675bdfbc3dd370876f78ca
emjc-github <enrique.johnsonchumbes@hgs.hiddengeniusproject.org>
2024-11-14 16:59:22 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,4 +1,4 @@
-document.getElementById("myButton").addEventListener('click', function (){
+document.getElementById("Clicker").addEventListener('click', function (){
 paragraph = document. createElement ('p');
 paragraph. textContent = "DON'T STOP CLICKING THE BUTTON!!!";
-document. body.appendChild (paragraph) ;
+document. body.Clicker (paragraph) ;
